9b1a1a8d1af383531f6b8bde0155719a72c57698
eminsamur <eminsamur00@gmail.com>
2024-03-26 15:45:23 +0300

    Emin

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -1,4 +1,3 @@
-
 export default function TitleComp({
   title,
   description,
@@ -8,17 +7,13 @@ export default function TitleComp({
 }) {
   return (
     <div
-      className={`${'font-fontspring'} titleBack font-satoshi
+      className={`${"font-fontspring"} titleBack font-satoshi
        flex items-center gap-6 w-full p-6`}
     >
       <div className="w-1/3">
-        <h1 className={`text-3xl ${'font-fontspringBold'}`}>
-          {title}
-        </h1>
+        <h1 className={`text-3xl ${"font-fontspringBold"}`}>{title}</h1>
       </div>
-      <p className="w-2/3 flex text-base font-normal">
-        {description}
-      </p>
+      <p className="w-2/3 flex text-base font-normal">{description}</p>
     </div>
   );
 }
